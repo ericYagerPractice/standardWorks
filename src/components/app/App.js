2ef9981cd3c3c9e2892eb9262d064d5840b643fa
Eric Yager <eyager@amazon.com>
2020-09-30 12:24:59 -0500

    update aws-exports

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,7 +1,7 @@
 /* src/App.js */
 import React from 'react';
 import Amplify from 'aws-amplify';
-import awsExports from "../aws-exports";
+import awsExports from "../../aws-exports";
 import { withAuthenticator} from '@aws-amplify/ui-react';
 import Header from '../features/header'
 
